68acca8c3ad078d06fe8968c79831574248b3fb7
Levis Kim <122298089+Orekihotarou-k@users.noreply.github.com>
2023-11-16 11:24:56 +0100

    code update

--- app.js
+++ app.js
@@ -5,41 +5,35 @@ const error = document.querySelector(".error")
 
 var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
 
-document.addEventListener("invalid", (e) => {
+document.addEventListener('invalid', (function(){
+    return function(e) {
+
+        e.preventDefault();
+
+        error.style.visibility = "visible"
+
+    };
+})(), true);
+
+function emailSuccess(){
+    error.style.visibility = "hidden"
+}
+
+function emailError() {
+    error.style.visibility = "visible"
+}
+
+form.addEventListener("submit", (e) => {
+
     e.preventDefault()
+
+    let emailValue = emailInput.value
+
+    let isValidEmail = emailRegex.test(emailValue)
+
+    if (isValidEmail/true ){
+        emailSuccess()
+    } else {
+        emailError()
+    }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// form.addEventListener("submit", (e) => {
-
-//     e.preventDefault()
-//     error.style.visibility = "visible"
-
-//     let emailValue = emailInput.value
-
-//     let isValidEmail = emailRegex.test(emailValue)
-
-//     if (isValidEmail === false ){
-//         console.log("Try again mudafoka")
-//     }
-// })
